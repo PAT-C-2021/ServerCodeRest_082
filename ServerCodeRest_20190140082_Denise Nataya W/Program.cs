2948e5ee03a006e3fe94a9266fff0d281bcae63b
dna2121 <dnatayawibisono@gmail.com>
2021-12-31 15:12:34 +0700

    Add project files.

---
+++ ServerCodeRest_20190140082_Denise Nataya W/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20190140082_Denise_Nataya_W;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+
+namespace ServerCodeRest_20190140082_Denise_Nataya_W
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            Uri address = new Uri("http://localhost:1907/Mahasiswa");
+            WebHttpBinding binding = new WebHttpBinding();
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY), address);
+                hostObjek.AddServiceEndpoint(typeof(ITI_UMY), binding, "");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                hostObjek.Description.Behaviors.Add(smb);
+                Binding mexbinding = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObjek.AddServiceEndpoint(typeof(IMetadataExchange), mexbinding, "mex");
+
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObjek.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObjek.Open();
+                Console.WriteLine("server ready...");
+                Console.ReadLine();
+                hostObjek.Close();
+
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
